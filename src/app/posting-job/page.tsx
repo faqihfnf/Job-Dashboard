86bd02e21f0cd9c9a47b32b2bf6f66f2d2425028
faqihfnf <faqih.fnf@gmail.com>
2024-09-08 22:02:24 +0700

    menyelesaikan post job page

--- src/app/posting-job/page.tsx
+++ src/app/posting-job/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { jobFormSchema } from "@/lib/form-schema";
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
@@ -13,19 +13,28 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { JOBTYPES } from "@/constant";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import InputSkills from "@/components/organisms/InputSkills";
+import CKEditor from "@/components/organisms/CKEditor";
+import InputBenefits from "@/components/organisms/InputBenefits";
+import { Button } from "@/components/ui/button";
 
 interface PostJobPageProps {}
 const PostJobPage: FC<PostJobPageProps> = ({}) => {
+  const [editorLoaded, setEditorLoaded] = useState(false);
   const form = useForm<z.infer<typeof jobFormSchema>>({
     resolver: zodResolver(jobFormSchema),
     defaultValues: {
       requiredSkills: [],
+      benefits: [],
     },
   });
 
   const onSubmit = (val: z.infer<typeof jobFormSchema>) => {
     console.log(val);
   };
+
+  useEffect(() => {
+    setEditorLoaded(true);
+  }, []);
 
   return (
     <div>
@@ -52,6 +61,7 @@ const PostJobPage: FC<PostJobPageProps> = ({}) => {
                   <FormControl>
                     <Input className="w-[450px]" placeholder="e.g Software Engineer" {...field} />
                   </FormControl>
+                  <FormMessage />
                   <FormDescription>Minimal 10 characters</FormDescription>
                 </FormItem>
               )}
@@ -96,10 +106,11 @@ const PostJobPage: FC<PostJobPageProps> = ({}) => {
                     <FormControl>
                       <Input className="w-full" placeholder="Rp.5000.000,-" {...field} />
                     </FormControl>
+                    <FormMessage />
                   </FormItem>
                 )}
               />
-              <span className="text-slate-400 text-center">to</span>
+              <span className="text-slate-400 text-center flex flex-auto justify-center ">to</span>
               <FormField
                 control={form.control}
                 name="salaryTo"
@@ -108,6 +119,7 @@ const PostJobPage: FC<PostJobPageProps> = ({}) => {
                     <FormControl>
                       <Input className="w-full" placeholder="Rp.10.000.000,-" {...field} />
                     </FormControl>
+                    <FormMessage />
                   </FormItem>
                 )}
               />
@@ -142,9 +154,38 @@ const PostJobPage: FC<PostJobPageProps> = ({}) => {
           </FieldInput>
           {/*//! Form Category End */}
 
+          {/*//? Form Required Sklills Start */}
           <FieldInput title="Required Skills" subtitle="Add required skills for the job">
             <InputSkills form={form} />
           </FieldInput>
+          {/*//? Form Required Sklills End */}
+
+          {/*//* Form Job Description Start */}
+          <FieldInput title="Job Description" subtitle="Enter your job description">
+            <CKEditor form={form} name="jobDescription" editorLoaded={editorLoaded} />
+          </FieldInput>
+          <FieldInput title="Responsibilities" subtitle="Outline the core responsibilities of the position">
+            <CKEditor form={form} name="responsibility" editorLoaded={editorLoaded} />
+          </FieldInput>
+          <FieldInput title="Who You Are" subtitle="Add your preferred candidate qualifications">
+            <CKEditor form={form} name="whoYouAre" editorLoaded={editorLoaded} />
+          </FieldInput>
+          <FieldInput title="Nice To Haves" subtitle="Add nice to haves skill for the role to encourage a more diserve set of candidates to apply">
+            <CKEditor form={form} name="niceToHaves" editorLoaded={editorLoaded} />
+          </FieldInput>
+          {/*//* Form Job Description Start */}
+
+          {/*//! Form Benefits Start */}
+          <FieldInput title="Perks and Benefits" subtitle="Encourage more people to apply by sharing the attractive rewards and benefits">
+            <InputBenefits form={form} />
+          </FieldInput>
+          {/*//! Form Benefits End */}
+
+          <div className="flex justify-end">
+            <Button className="bg-emerald-500 hover:bg-emerald-600" size={"lg"}>
+              Review Job
+            </Button>
+          </div>
         </form>
       </Form>
     </div>
--- src/lib/form-schema.ts
+++ src/lib/form-schema.ts
@@ -2,16 +2,16 @@ import { JOBTYPES } from "@/constant";
 import { z } from "zod";
 
 export const jobFormSchema = z.object({
-  roles: z.string({ required_error: "Role is required" }).min(3, { message: "Job Title must be at least 3 characters" }),
+  roles: z.string({ required_error: "Job title is required" }).min(10, { message: "Job Title must be at least 10 characters" }),
   jobType: z.enum(JOBTYPES, { required_error: "Job Type is required" }),
   salaryFrom: z.string({ required_error: "Salary From is required" }),
-  salaryTo: z.string({ required_error: "Salary From is required" }),
+  salaryTo: z.string({ required_error: "Salary to is required" }),
   categoryId: z.string({ required_error: "Category is required" }),
   requiredSkills: z.string().array().nonempty({ message: "Required Skills must be at least 1 skill" }),
   jobDescription: z.string({ required_error: "Job Description is required" }).min(20, { message: "Job Description must be at least 20 characters" }),
-  responsibility: z.string({ required_error: "Job Description is required" }).min(10, { message: "Job Description must be at least 10 characters" }),
-  whoYouAre: z.string({ required_error: "Job Description is required" }).min(10, { message: "Job Description must be at least 10 characters" }),
-  niceToHaves: z.string({ required_error: "Job Description is required" }).min(10, { message: "Job Description must be at least 10 characters" }),
+  responsibility: z.string({ required_error: "responsibilities is required" }).min(10, { message: "responsibilities must be at least 10 characters" }),
+  whoYouAre: z.string({ required_error: "Who You Are is required" }).min(10, { message: "Who You Are must be at least 10 characters" }),
+  niceToHaves: z.string({ required_error: "Nice to Haves is required" }).min(10, { message: "Nice to Haves must be at least 10 characters" }),
   benefits: z
     .object({
       benefit: z.string(),
