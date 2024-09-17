06366aea1106f61f419529fb2343a330967fa588
faqihfnf <faqih.fnf@gmail.com>
2024-09-18 00:26:36 +0700

    connect sign in to database and auth with next-auth

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,13 +1,18 @@
-import Sidebar from "@/components/layouts/Sidebar";
 import "../globals.css";
-import type { Metadata } from "next";
 import { Epilogue } from "next/font/google";
-import Header from "@/components/layouts/Header";
 import { Toaster } from "@/components/ui/toaster";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
 const epilogue = Epilogue({ subsets: ["latin"] });
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  const session = await getServerSession(authOptions);
+  if (session !== null) {
+    return redirect("/");
+  }
+
   return (
     <html lang="en">
       <body className={epilogue.className}>
